c232b8184a93b65921c45e627f30cce47f21ff7d
Eddie Chung <designered@gmail.com>
2019-04-23 11:26:00 +0200

    added login / logout navbar logic

---
+++ project-map/routes/profile.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const router = express.Router();
+const User = require("../models/User");
+
+module.exports = router;

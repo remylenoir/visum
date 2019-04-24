91ad4190c70cd9092e332a715f0ced1c997085e1
Eddie Chung <designered@gmail.com>
2019-04-25 00:26:56 +0200

    added profile page styling, show favourite filters logic

--- project-map/routes/profile.js
+++ project-map/routes/profile.js
@@ -6,10 +6,8 @@ const User = require("../models/User");
 
 router.get("/profile", (req, res, next) => {
   if (req.isAuthenticated()) {
-    User.findOne({ _id: req.user._id }).then(user => {
-      res.render("auth/profile", { user, isAuthenticated: true });
-      console.log(user);
-    });
+    const { profileImgPath, username, mapLayer } = req.user;
+    res.render("auth/profile", { profileImgPath, username, mapLayer, isAuthenticated: true });
   }
 });
 

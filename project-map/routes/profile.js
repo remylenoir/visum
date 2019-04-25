e113e2acf6eb9fc74245fc90787c8d30757207bc
Eddie Chung <designered@gmail.com>
2019-04-25 15:30:28 +0200

    fixed profile not login routing

--- project-map/routes/profile.js
+++ project-map/routes/profile.js
@@ -8,6 +8,9 @@ router.get("/profile", (req, res, next) => {
   if (req.isAuthenticated()) {
     const { profileImgPath, username, mapLayer } = req.user;
     res.render("auth/profile", { profileImgPath, username, mapLayer, isAuthenticated: true });
+  } else {
+    res.status(404);
+    res.render("not-found");
   }
 });
 
